215b1a6f8e81abbdb92d4fcf1645bb913180bb2a
rogier <rogier.burger@gmail.com>
2021-06-05 21:49:54 +0200

    found loops

--- Warmtenet/branched_tree/cutting_loops.py
+++ Warmtenet/branched_tree/cutting_loops.py
@@ -11,46 +11,49 @@ from utils import split_line_with_points
 crs = {'init': 'epsg:28992'}
 
 
-points_with_house_and_source = gpd.read_file('../data/loops_district/Aansluitpunten.geojson')
-points_with_house = points_with_house_and_source[points_with_house_and_source['pandidentificatie'] != 'BRON']
-points_with_house.loc[:, 'pandidentificatie'] = [str(p[1:]) for p in points_with_house['pandidentificatie']]
-index_bron = points_with_house_and_source[points_with_house_and_source['pandidentificatie'] == 'BRON'].index.values[0]
-
-junctions = gpd.read_file('../data/loops_district/Kruispunten.geojson')
-points = pd.concat([points_with_house_and_source, junctions], ignore_index=True, sort=False)
+# points_with_house_and_source = gpd.read_file('../data/loops_district/Aansluitpunten.geojson')
+# points_with_house = points_with_house_and_source[points_with_house_and_source['pandidentificatie'] != 'BRON']
+# points_with_house.loc[:, 'pandidentificatie'] = [str(p[1:]) for p in points_with_house['pandidentificatie']]
+# index_bron = points_with_house_and_source[points_with_house_and_source['pandidentificatie'] == 'BRON'].index.values[0]
+#
+# junctions = gpd.read_file('../data/loops_district/Kruispunten.geojson')
+# points = pd.concat([points_with_house_and_source, junctions], ignore_index=True, sort=False)
 roads = gpd.read_file('../data/loops_district/Wegen.geojson')
+#
+# # points_unique_geometry.geometry = [geom.centroid for geom in points_unique_geometry.geometry]
+#
+# #see what point are so near to one another that they should be treated as one
+# point_same = {}
+# unique_points = []
+# for index, point in points.iterrows():
+#     point_list = []
+#     if index not in unique_points:
+#         for index2, point2 in points.iterrows():
+#             if point.geometry.distance(point2.geometry) < 1e-1 and index != index2:
+#                 point_list.append(index2)
+#                 if index2 not in unique_points:
+#                    unique_points.append(index2)
+#     point_same[index] = point_list
+#
+# # get all panden together on that single point
+# panden={}
+# to_remove = []
+# for key in point_same.keys():
+#     if point_same[key]:
+#        andere_panden = [i for i in points.loc[point_same[key], 'pandidentificatie'].values if i is not None]
+#        eigen_pand = points.loc[key,  'pandidentificatie']
+#        if eigen_pand is not None:
+#            andere_panden.append(eigen_pand)
+#        panden[key] = str(andere_panden)
+#        to_remove.extend(point_same[key])
+#     else:
+#        panden[key] = "[]"
+#
+# alle_panden = pd.Series(panden, name='alle_panden')
+# points_unique_geometry = pd.concat([points, alle_panden], axis=1)
+# points_unique_geometry = points_unique_geometry.drop(to_remove).reset_index()
 
-# points_unique_geometry.geometry = [geom.centroid for geom in points_unique_geometry.geometry]
-
-#see what point are so near to one another that they should be treated as one
-point_same = {}
-unique_points = []
-for index, point in points.iterrows():
-    point_list = []
-    if index not in unique_points:
-        for index2, point2 in points.iterrows():
-            if point.geometry.distance(point2.geometry) < 1e-1 and index != index2:
-                point_list.append(index2)
-                if index2 not in unique_points:
-                   unique_points.append(index2)
-    point_same[index] = point_list
-
-# get all panden together on that single point
-panden={}
-to_remove = []
-for key in point_same.keys():
-    if point_same[key]:
-       andere_panden = list(points.loc[point_same[key], 'pandidentificatie'].values.astype(str))
-       eigen_pand = points.loc[key,  'pandidentificatie']
-       andere_panden.append(eigen_pand)
-       panden[key] = andere_panden
-       to_remove.extend(point_same[key])
-    else:
-       panden[key] = []
-alle_panden = pd.Series(panden, name='alle_panden')
-points_unique_geometry = pd.concat([points, alle_panden], axis=1)
-points_unique_geometry = points_unique_geometry.drop(to_remove).reset_index()
-
+points_unique_geometry = gpd.read_file('./point_unique_geometry.geojson')
 
         # see what roads contain multiple points (roads that do not go only from one to another point)
 long_roads = {}
@@ -122,6 +125,61 @@ for i in range(len(points_in_road_short)):
 
 # cut_loops
 print(p2p)
+
+index_bron =  points_unique_geometry[points_unique_geometry['pandidentificatie'] == 'BRON'].index.values[0]
+
+#find loops
+loops={}
+x=0
+loops[x] = [index_bron]
+real_loops = []
+active_keys = [x]
+finished_keys = []
+finished_points = []
+while len(active_keys) > 0:
+    for key in active_keys:
+        p_conn = np.array(p2p[loops[key][-1]])
+        if len(p_conn) == 1 and loops[key][-1] != index_bron:
+            finished_points.extend(loops[key])
+            finished_points = list(set(finished_points))
+            active_keys.remove(key)
+            finished_keys.append(key)
+
+        else:
+            if len(p_conn) == 1:
+                loops[key].append(p_conn[0])
+            else:
+                for i, p_index in enumerate(p_conn[p_conn!=loops[key][-2]]):
+                    if i == 0:
+                        if (p_index not in loops[key]) and (p_index not in finished_points):
+                            loops[key].append(p_index)
+                        else:
+                            if p_index in loops[key]:
+                                finished_points.extend(loops[key][loops[key].index(p_index):])
+                                finished_points = list(set(finished_points))
+                                real_loops.append(loops[key][loops[key].index(p_index):] + [p_index])
+                            loops[key].append(p_index)
+
+                            active_keys.remove(key)
+                            finished_keys.append(key)
+                    if i>0:
+                        x += 1
+                        loop_orig = loops[key][:-1]
+                        loops[x] = loop_orig + [p_index]
+                        if (p_index not in loop_orig) and (p_index not in finished_points):
+                            active_keys.append(x)
+                        else:
+                            finished_keys.append(x)
+                            if p_index in loop_orig:
+                                finished_points.extend(loop_orig[loop_orig.index(p_index):])
+                                finished_points = list(set(finished_points))
+                                real_loops.append(loop_orig[loop_orig.index(p_index):] + [p_index])
+
+unique_loops = set(tuple(i) for i in real_loops)
+
+f, ax = plt.subplots()
+for loop in unique_loops:
+
 
 
 
